3d7259c703a7bbef4bef75937c9d08b1db0f03f0
Dmitriy Angve <kirirch129@gmail.com>
2023-02-03 12:47:08 +0300

    Add messages for actions with DB

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const container = document.querySelector(".container");
+const message = cme2(container, "div", "Message Area");
 const val1 = document.querySelector(".val");
 const output = document.querySelector(".output");
 const baseurl = "http://localhost:3000/";
 
 const btn1 = document.querySelector(".btn");
-const btn2 = cme(container, "button", "Load Posts");
+const btn2 = cme2(container, "button", "Load Posts");
 btn1.textContent = "Create New";
 
 window.addEventListener("DOMContentLoaded", getAllPosts);
@@ -13,9 +14,10 @@ btn1.addEventListener("click", addPost);
 btn2.addEventListener("click", getAllPosts);
 
 function addPost(e) {
-  console.log("Ready");
+  console.log("ready");
   e.preventDefault();
   const title = val1.value || "default title";
+  val1.value = "";
   const url = baseurl + "posts";
   const body = {
     title: title,
@@ -28,12 +30,17 @@ function addPost(e) {
       "Content-type": "application/json; charset=UTF-8",
     },
   };
-  console.log(opts);
+  // console.log(opts);
   fetch(url, opts)
     .then((res) => res.json())
     .then((data) => {
-      console.log(data);
+      makeItem(data);
+      myMessage(`New Item Made id = ${data.id}`);
     });
+}
+
+function myMessage(html) {
+  message.innerHTML = html;
 }
 
 function getAllPosts(e) {
@@ -58,6 +65,7 @@ function makeItem(el) {
   console.log(el);
   const main = cme(output, "div", "");
   main.classList.add("box");
+  const myID = cme(main, "div", el.id);
   const in1 = cme(main, "input", "");
   in1.value = el.title;
   const in2 = cme(main, "input", "");
@@ -72,9 +80,11 @@ function makeItem(el) {
       author: in2.value,
     };
     updateItem(json, el.id);
+    myMessage(`Updated ${el.id}`);
   });
   bt2.addEventListener("click", (e) => {
     // Button for DELETE
+    myMessage(`Deleted ${el.id}`);
     const url = baseurl + "posts/" + el.id;
     fetch(url, { method: "DELETE" });
     main.remove();
@@ -96,6 +106,14 @@ function updateItem(json, id) {
     .then((data) => {
       console.log(data);
     });
+}
+
+function cme2(parent, typeEle, html) {
+  // cme - createMyElement
+  const el = document.createElement(typeEle);
+  parent.prepend(el);
+  el.innerHTML = html;
+  return el;
 }
 
 function cme(parent, typeEle, html) {
